ac0e17a1b5c9c725d4162c108952e869bfe0aaf5
Uy Binh <uybinh@gmail.com>
2018-03-01 05:29:21 +0700

    Finish calculator exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,35 @@
-#write your code here
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(numbers)
+  sum = 0
+  numbers.each do |x|
+    sum += x
+  end
+  sum
+end
+
+def multiply(*numbers)
+  multiply = 1
+  numbers.each do |x|
+    multiply *= x
+  end
+  multiply
+end
+
+def power(num, pow)
+  num**pow
+end
+
+def factorial(number)
+  if number < 2
+    return 1
+  end
+  number * factorial(number - 1)
+end
+
